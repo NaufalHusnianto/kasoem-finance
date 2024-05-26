ad9391ef915cb2d32d428bb02451271b7abdaa55
NaufalHusnianto <husniantonaufal@gmail.com>
2024-05-26 10:11:34 +0700

    Kategori, Pelanggan, dan Produk

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CustomerResource\Pages;
+use App\Filament\Resources\CustomerResource\RelationManagers;
+use App\Models\Customer;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Group::make([
+                    Forms\Components\TextInput::make('name')
+                        ->label('Nama')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('age')
+                        ->label('Umur')
+                        ->required()
+                        ->numeric(),
+                    Forms\Components\Select::make('gender')
+                        ->label('Jenis Kelamin')
+                        ->options([
+                            'Laki-Laki' => 'Laki-Laki',
+                            'Perempuan' => 'Perempuan',
+                        ])
+                        ->required(),
+                    Forms\Components\TextArea::make('address')
+                        ->label('Alamat')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('phone')
+                        ->label('Nomor Telepon')
+                        ->numeric()
+                        ->required()
+                        ->maxLength(255),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('age')
+                    ->label('Umur')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('gender')
+                    ->label('Jenis Kelamin')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->label('Alamat')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->label('Nomor Telepon')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,6 @@
+<p class="font-bold text-xl">
+    KASOEM
+    <span class="text-primary" style="color: mediumseagreen">
+        FINANCE
+    </span>
+</p>
